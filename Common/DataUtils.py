e711c0f663237bb220a7ff450930651fa1d6a8a2
Pradnyesh Gore <pgore@gmail.com>
2021-12-15 12:00:15 -0800

    Complete P11.1, AOC

--- Common/DataUtils.py
+++ Common/DataUtils.py
@@ -44,11 +44,24 @@ def build_map(fp, cast_type = None):
     return DataMap(map_elements, width, height)
 
 ####
-def get_map_string(map, element_separator = ''):
+def get_map_string(map, element_separator = '', format_width = None):
     s = ''
     for y in range(map.height):
         for x in range(map.width):
-            s += str(map.get_element(x,y))
+            element = map.get_element(x,y)
+            if format_width:
+                s +=  f'{element:{format_width}}'    
+            else:
+                s +=  str(map.get_element(x,y))
             s += element_separator
         s += '\n'
     return s
+
+####
+# no diagonals considered
+def get_neighbor_pos(data_map, x, y, diagonal=True):
+    neighbor_pos = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]    
+    if diagonal:
+        neighbor_pos += [(x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
+    neighbor_pos = list(pos for pos in neighbor_pos if data_map.is_valid_pos(pos[0], pos[1])) # remove None elements
+    return neighbor_pos
---
+++ P11/process_p11.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import pathlib
+
+# Add Common to sys path
+sys.path.append(os.path.join(pathlib.Path(__file__).parent.resolve(),'..', 'Common'))
+
+import DataUtils
+
+###############
+
+def increment_map_for_flash(data_map):
+    flash_positions = []
+    for y in range(data_map.height):
+        for x in range(data_map.width):
+            inc = data_map.get_element(x, y) + 1
+            if inc > 9:
+                flash_positions.append((x, y))
+            data_map.set_element(x, y, inc)
+    return flash_positions
+
+def process_flash(data_map, flash_positions):
+    while len(flash_positions) > 0:
+        pos = flash_positions.pop(0)
+        # print(f'Process flash {pos}')
+        neighbor_pos = DataUtils.get_neighbor_pos(data_map, pos[0], pos[1])  
+        for neighbor in neighbor_pos:
+            element = data_map.get_element(neighbor[0], neighbor[1])
+            if element == 9 and neighbor not in flash_positions:
+                flash_positions.append(neighbor)
+            data_map.set_element(neighbor[0], neighbor[1], element + 1)
+        # print(DataUtils.get_map_string(data_map, element_separator=' ', format_width=2))
+            
+def reset_flashed(data_map):
+    flashed_pos = []
+    for y in range(data_map.height):
+        for x in range(data_map.width):
+            if data_map.get_element(x, y) > 9:
+                data_map.set_element(x, y, 0)
+                flashed_pos.append((x,y))
+    return flashed_pos
+
+def process_step(data_map):    
+    # print(f'---------------------- Step ----------------------')
+    # print(DataUtils.get_map_string(data_map, element_separator=' ', format_width=2))
+    
+    # print(f'Increment')
+    flash_positions = increment_map_for_flash(data_map)
+    # print(DataUtils.get_map_string(data_map, element_separator=' ', format_width=2))
+    
+    
+    process_flash(data_map, flash_positions)
+    
+    # print(f'Reset')
+    flashed_pos = reset_flashed(data_map)    
+    # print(DataUtils.get_map_string(data_map, element_separator=' ', format_width=2))
+    return len(flashed_pos)
+
+###############
+
+def process():
+    filePath = '/Users/pgore/dev/AOC21/P11/input/input1.txt'
+    step_count = 100
+    flash_count = 0
+    with open(filePath) as fp:
+        data_map = DataUtils.build_map(fp, cast_type=int)
+        for index in range(step_count):
+            flash_count += process_step(data_map)
+            # print(f'============= Step {index + 1} ==============')
+            # print(DataUtils.get_map_string(data_map, element_separator='', format_width=1))
+        print(f'After {step_count} steps there have been {flash_count} flashes')
+        
+        
+process()
