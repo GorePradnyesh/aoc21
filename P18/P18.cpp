79a8df6e9b48503ff3a227d14dfed773ae4193c0
Pradnyesh Gore <gore.pradnyesh@gmail.com>
2021-12-26 16:37:28 -0800

    Add token processing for P18
    converts line to binary tree with values as leaf nodes

--- P18/P18.cpp
+++ P18/P18.cpp
@@ -3,6 +3,78 @@
 
 #include "common/FileUtils.h"
 
+
+namespace
+{
+	// TODO: tokenization code assumes int data type
+	// TODO: tokenization code assumes all ints are [0,9]. will not work for values above 10
+	// TODO: tokenization also assumes well formed lines ( complete, balanced and uncorrupted )
+	/*
+	** Stack based processing of the tokens encountered
+	*/
+	bool ProcessLine(const std::string& inLine, NodePtr<int>& outRootNode)
+	{
+		std::stack<char> paran_stack; // holds '['
+		std::stack<NodePtr<int>> node_stack;
+	
+		std::string::const_iterator it;
+		for(it = inLine.begin(); it != inLine.end(); it++)
+		{
+			char token = *it;
+			if(token == '[')
+			{
+				paran_stack.push(token);
+			}
+			else if(isdigit(token))
+			{
+				auto value = std::atoi(&token);
+				auto node = std::make_shared<Node<int>>(value);
+				node_stack.push(node);
+			}
+			else if(token == ']')
+			{
+				if(node_stack.size() == 1)
+				{
+					// we might have reached the end of string
+					// OR we have balanced parans. Do nothing in this block
+				}
+				else
+				{
+					auto op2 = node_stack.top(); node_stack.pop();
+					auto op1 = node_stack.top(); node_stack.pop();
+					
+					//NOTE the order of operand args to the node/
+					auto nonLeafNode = std::make_shared<Node<int>>(op1, op2, 0);
+					node_stack.push(nonLeafNode);
+				}
+				paran_stack.pop();
+			}
+			else if(token == ',')
+			{
+				; // valid char. Ignore.
+			}
+			else
+			{
+				std::cout << "Exiting . Unexpected token : " << token << "\n";
+				return false;
+			}
+		}
+		if(node_stack.size() == 1)
+		{
+			// remaining node is our root node
+			outRootNode = node_stack.top(); node_stack.pop();
+			return true;
+		}
+		else
+		{
+			std::cout << "Stack had more than 1 element remaining. Incorrect input. \n";
+			return false;
+		}
+	}
+	
+} // unnamed
+
+
 namespace P18
 {
 
@@ -21,6 +93,26 @@ void Process()
 		std::cout << "Exiting puzzle. Empty file encountered : " << filePath << "\n";
 		return;
 	}
+	
+	// Check line processing
+	auto line = lines.front();
+	NodePtr<int> rootNode;
+	bool construction_success = ProcessLine(line, rootNode);
+	if(construction_success)
+	{
+		// std::cout << *rootNode << std::endl;
+		PrintNode(rootNode);
+	}
+		
+	// Example Usage
+	/*
+	NodePtr<int> node_l = std::make_shared<Node<int>>(45);
+	NodePtr<int> node_r = std::make_shared<Node<int>>(46);
+	NodePtr<int> node_root = std::make_shared<Node<int>>(node_l, node_r, 0);
+	
+	PrintNode(node_root);
+	*/
+	
 }
 
 } // namespace P18
--- native/include/common/BinaryTree.h
+++ native/include/common/BinaryTree.h
@@ -3,8 +3,9 @@
 **
 */
 
-#include <ostream>
+#include <iostream>
 #include <memory>
+#include <sstream>
 
 template <typename T>
 struct Node
@@ -14,14 +15,51 @@ public:
 	:mData(inData)
 	{}
 	
+	Node(
+		const std::shared_ptr<Node<T>> inLeftNode,
+		const std::shared_ptr<Node<T>> inRightNode,
+		const T& inData)
+	:mLeft(inLeftNode),
+	mRight(inRightNode),
+	mData(inData)
+	{}
+	
+	
 	T mData;
-	std::shared_ptr<Node> mLeft;
-	std::shared_ptr<Node> mRight;
+	std::shared_ptr<Node<T>> mLeft;
+	std::shared_ptr<Node<T>> mRight;
 	
 	// ?
-	std::shared_ptr<Node> mParent;
+	std::shared_ptr<Node<T>> mParent;
 };
 
+template <typename T>
+using NodePtr = std::shared_ptr<Node<T>>;
+
+template <typename T>
+bool AddLeft(const NodePtr<T>& node, const NodePtr<T>& inOther)
+{
+	if(node->mLeft)
+	{
+		std::cout << "Bad Op. Node already contains left \n";
+		return false;
+	}
+	node->mLeft = inOther;
+	return false;
+}
+
+
+template <typename T>
+bool AddRight(const NodePtr<T>& node, const NodePtr<T>& inOther)
+{
+	if(node->mRight)
+	{
+		std::cout << "Bad Op. Node already contains left \n";
+		return false;
+	}
+	node->mRight = inOther;
+	return false;
+}
 
 /*
 **
@@ -31,5 +69,42 @@ std::ostream& operator<<(std::ostream& os, const Node<T>& inNode)
 {
 	os << inNode.mData ;
 	return os;
+}
+
+/*
+**
+*/
+template<typename T>
+void GetNodeStream(const NodePtr<T>& inNode, std::stringstream& ss)
+{
+	if(inNode->mData)
+	{
+		ss << inNode->mData;
+	}
+	else
+	{
+		ss << "[";
+		if(inNode->mLeft)
+		{
+			GetNodeStream(inNode->mLeft, ss);
+			ss << ",";
+		}
+		if(inNode->mRight)
+		{
+			GetNodeStream(inNode->mRight, ss);
+		}
+		ss << "]";
+	}
+}
+
+/*
+**
+*/
+template<typename T>
+void PrintNode(const NodePtr<T>& inNode)
+{
+	std::stringstream ss;
+	GetNodeStream(inNode, ss);
+	std::cout << ss.str() << "\n";
 }
 
